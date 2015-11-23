af3cbd5cb43d1bf5e3f7b409ccdc5d3b5b1f09d8
tgaff <tgaff@alumni.nd.edu>
2015-11-23 11:36:26 -0800

    add another way to run the code using index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+var myEach = require('./myEach');
+var myMap = require('./myEach');
+var myReduce = require('./myEach');
+/* *********************************************************************
+  You can edit this file
+  It will make use of your code in myEach.js, myMap.js and myReduce.js
+  To run it on the console do: node index.js
+***********************************************************************/
+
+var numArray = [0,1,10,100,1000];
+
+
+
+/* myEach */
+
+//
+// myEach(numArray, function(element, index, arr) {
+//   console.log('inside myEach', element, index, arr);
+// });
+
+
+
+
+/* myMap */
+
+//
+// var myMapResult = myMap(numArray, function(element, index, arr) {
+//   console.log('inside myMap', element, index, arr);
+// });
+// console.log('myMapResult', myMapResult);
+
+
+console.log("the end");
